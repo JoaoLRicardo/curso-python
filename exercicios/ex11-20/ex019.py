9bf0720127d6d75363a9640fbe364f58fc91336a
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-01 22:01:58 -0300

    Update ex019.py

---
+++ exercicios/ex11-20/ex019.py
@@ -0,0 +1,8 @@
+from random import choice
+n1 = str(input('Primeiro aluno: ')).title().strip()
+n2 = str(input('Segundo aluno: ')).title().strip()
+n3 = str(input('Terceiro aluno: ')).title().strip()
+n4 = str(input('Quarto aluno: ')).title().strip()
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
