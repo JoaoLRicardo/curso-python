73f9244c83ce49c18d40a0b0123d3dc42713e6a6
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-24 16:30:38 -0300

    Update ex061.py

--- exercicios/ex61-70/ex061.py
+++ exercicios/ex61-70/ex061.py
@@ -6,3 +6,11 @@ def escreva(msg):
 
 
 escreva('Gerador de PA')
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f'{termo} ', end='')
+    termo += razao
+    cont += 1
