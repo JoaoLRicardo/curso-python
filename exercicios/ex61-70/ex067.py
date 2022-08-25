26ec82a203cff6f6001d8de773f44ac4b6c77993
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-25 16:21:54 -0300

    Update ex067.py

--- exercicios/ex61-70/ex067.py
+++ exercicios/ex61-70/ex067.py
@@ -1,7 +1,9 @@
 while True:
-    num = int(input('Quer ver a tabuada de qual valor? '))
-    if num < 0:
+    num = int(input('Quer ver a tabuada de qual valor [0 para SAIR]? '))
+    print()
+    if num == 0:
         break
     for c in range(1, 11):
         print(f'{num} x {c:2} = {num * c:2}')
-print('PROGAMA TABUADA ENCERRADO.')
+    print()
+print('PROGAMA TABUADA ENCERRADO COM SUCESSO.')
