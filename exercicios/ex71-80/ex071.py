a4fdfcd0ea440da6564f83a8ab142917d9378cc4
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-26 14:00:32 -0300

    Update ex071.py

--- exercicios/ex71-80/ex071.py
+++ exercicios/ex71-80/ex071.py
@@ -1,4 +1,30 @@
-import emoji
+def escreva(msg):
+    tam = len(msg) + 4
+    print(tam * '=')
+    print(f'  {msg}')
+    print(tam * '=')
 
 
-help(emoji)
+escreva('BANCO CEV')
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 20:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO DE CEV! Tenha um bom dia!')
