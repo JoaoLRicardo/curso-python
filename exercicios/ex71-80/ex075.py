e4202c430e74d6d8ec8cc2126ff925e33685128b
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-26 16:20:58 -0300

    Update ex075.py

--- exercicios/ex71-80/ex075.py
+++ exercicios/ex71-80/ex075.py
@@ -1,14 +1,14 @@
 num = (int(input('Digite um número: ')),
-        int(input('Digite outro número: ')),
-        int(input('Digite mais número: ')),
-        int(input('Digite o último número: ')))
+       int(input('Digite outro número: ')),
+       int(input('Digite mais número: ')),
+       int(input('Digite o último número: ')))
 print(f'Você digitou os valores {num}')
 print(f'O valor 9 apareceu {num.count(9)} vezes')
 if 3 in num:
     print(f'O valor 3 apareceu na {num.index(3) + 1}ª posição')
 else:    
     print(f'O valor 3 não foi digitado em nenhuma posição')
-print('Os valores pares digitados forma', end='')
+print('Os valores pares digitados form ', end='')
 for n in num:
-    if n % 2 ==0:
-        print(n, end='')
+    if n % 2 == 0:
+        print(n, end=' ')
