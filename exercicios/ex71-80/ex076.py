287ed6f06f40fc50b020e92bb909cb56d97d005a
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-26 16:37:14 -0300

    Update ex076.py

--- exercicios/ex71-80/ex076.py
+++ exercicios/ex71-80/ex076.py
@@ -1,3 +1,22 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print(tam * '=')
+    print(f'  {msg}')
+    print(tam * '=')
+
+
 listagem = ('Lápis', 1.75,
             'Borracha', 2,
-            '')
+            'Caderno', 15.9,
+            'Estojo', 25,
+            'Transferidor', 4.2,
+            'Compasso', 9.99,
+            'Mochila', 120.22,
+            'Canetas', 22.3,
+            'Livro', 34.9)
+escreva('LISTAGEM DE PREÇOS')
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:<25}', end='')
+    else:
+        print(f'R${listagem[pos]:>7}')
