9b0a58f645de55f385185003dcb3ffdf39566924
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-08-26 14:59:59 -0300

    Update ex073.py

---
+++ exercicios/ex71-80/ex073.py
@@ -0,0 +1,24 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print(tam * '=')
+    print(f'  {msg}')
+    print(tam * '=')
+
+
+times = ('Palmeiras', 'Fluminense', 'Flamengo', 'Corinthians', 'Internacional',
+        'Athletico-PR', 'Atlético-MG', 'Santos', 'América-MG', 'Bragantino',
+        'Goiás', 'São Paulo', 'Fortaleza', 'Botafogo', 'Ceará', 'Cuiabá', 
+        'Avaí', 'Coritiba', 'Atlético-GO', 'Juventude')
+escreva('CLASSIFICAÇÃO BRASILEIRAO SÉRIE A')
+c = 0
+for t in times:
+    c += 1
+    if c < 5:
+        print(f'\033[32m{c}º {t}')
+    if c >= 5 and c <= 15:
+        print(f'\033[m{c}º {t}')
+    if c > 16:
+        print(f'\033[31m{c}º {t}')
+print('\033[m')
+print(f'Os 5 primeiros são {times[0:5]}')
+print(f'Os 4 últimos são {times[-4:]}')
