216a6c4cf33942b78889482c42f73387067a5ce6
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-19 13:27:09 -0300

    Update ex092.py
    code review

--- exercicios/ex91-100/ex092.py
+++ exercicios/ex91-100/ex092.py
@@ -7,7 +7,7 @@ dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
 if dados['ctps'] != 0:
     dados['contratacao'] = int(input('Ano de contratacão: '))
     dados['salario'] = float(input('Salário: R$'))
-    dados['aposentadoria'] = dados['idade'] + ((dados['contratacao'] + 35) - datetime.now().year)
+    dados['aposentadoria'] = (dados['idade']) + ((dados['contratacao'] + 35) - datetime.now().year)
 print('-=' * 30)
 for k, v in dados.items():
     print(f'  - {k} tem o valor {v}')
