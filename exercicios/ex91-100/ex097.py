e07c1c6c3cad451a7dc9736ccf974ccfb501cbe5
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-19 20:53:23 -0300

    Update ex097.py

---
+++ exercicios/ex91-100/ex097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print(tam * '~')
+    print(f'  {msg}')
+    print(tam * '~')
+
+
+escreva('João Luiz Ricardo')
+escreva('Curso de Python no Youtube')
+escreva('CeV')
