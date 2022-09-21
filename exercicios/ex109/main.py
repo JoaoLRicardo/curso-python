f9b328c5a2749ebf79e83d6d1770a2bae66587cf
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-21 00:53:56 -0300

    Update

--- exercicios/ex109/main.py
+++ exercicios/ex109/main.py
@@ -1,6 +1,7 @@
 from exercicios.ex109 import moeda
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
-print(f'Aumentando em 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}')
+print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Aumentando em 10%, temos {moeda.aumentar(p, 10, True)}')
+print(f'Diminuindo em 13%, temos {moeda.diminuir(p, 13, True)}')
