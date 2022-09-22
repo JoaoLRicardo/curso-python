80dab1e2fef4f70755dc0a363f83c02d9cd3d4f0
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-22 12:14:43 -0300

    Update ex113.py

--- exercicios/ex113.py
+++ exercicios/ex113.py
@@ -13,5 +13,21 @@ def leiaint(msg):
             return n
 
 
-num = leiaint('Digite um valor: ')
-print(f'O valor digitado foi {num}')
+def leiafloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número real válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print()
+            print('\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+num1 = leiaint('Digite um valor inteiro: ')
+num2 = leiafloat('Digite um valor real: ')
+print(f'O valor inteiro digitado foi {num1} e o real foi {num2}.')
