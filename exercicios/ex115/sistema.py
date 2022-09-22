e0eabe83bfd77687694638501843434302abd592
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-22 16:00:31 -0300

    Update ex115

---
+++ exercicios/ex115/sistema.py
@@ -0,0 +1,15 @@
+from exercicios.ex115.lib.interface import *
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Listar Pessoas', 'Sair do Sistema'])
+    if resposta == 1:
+        print('Opção 1')
+    elif resposta == 2:
+        print('Opção 2')
+    elif resposta == 3:
+        print('Opção 3')
+    elif resposta == 4:
+        print('Saindo do sistema...Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
